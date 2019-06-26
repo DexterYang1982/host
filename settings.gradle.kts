93070f49c6471e2c5d29477447ffc92fd7b37abe
Dexter Yang <dexter.yang@qq.com>
2019-06-26 19:24:32 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "host"
+includeFlat("exchange")
+includeFlat("repository-spring")
---
+++ src/main/kotlin/net/gridtech/host/HostApplication.kt
@@ -0,0 +1,11 @@
+package net.gridtech.host
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HostApplication
+
+fun main(args: Array<String>) {
+    runApplication<HostApplication>(*args)
+}
